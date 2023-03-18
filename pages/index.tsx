7b4a13cbc0febf451b1cd93dfcbbd9d96164df63
NemesisLW <86069181+NemesisLW@users.noreply.github.com>
2023-03-18 16:38:06 +0530

    Updated Index.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,11 +8,11 @@ import Script from "next/script";
 // const logo = require("logo.png");
 
 const Home = () => {
+  const [ask, setAsk] = useState("");
+
   const [age, setAge] = useState("");
   const [gender, setGender] = useState("");
   const [address, setAddress] = useState("");
-
-  const [ask, setAsk] = useState("");
 
   //API Call
   const [apiOutput, setApiOutput] = useState("");
@@ -27,11 +27,12 @@ const Home = () => {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ gender, age }),
+      body: JSON.stringify({ ask }),
     });
 
     const data = await response.json();
     const { output } = data;
+
     console.log("OpenAI replied....", output.text);
 
     setApiOutput(`${output.text}`);
@@ -51,84 +52,68 @@ const Home = () => {
       </Head>
       <div className="bgr">
         <div className="bgrin">
-          <div></div>
-
           <div className="container">
             <div className="animation">
               <span className="first">Therapist</span>
               <span className="slide">
-                <span className="second">AI</span>
+                <span className="second">.AI</span>
               </span>
             </div>
           </div>
         </div>
       </div>
       <div className="containerc">
-        <div className="header">
-          <div className="header-subtitle">
-            <div class="wrapper">
-              <ul class="dynamic-txts">
-                <li>
-                  <span>
-                    Your mental health superhero, always by your side.
-                  </span>
-                </li>
-                <li>
-                  <span>Are You feeling too depressed and hopeless?</span>
-                </li>
-                <li>
-                  <span>
-                    There is Our THERAPIST.Ai to support and help You.
-                  </span>
-                </li>
-              </ul>
+        <div className="header-subtitle">
+          <div className="wrapper">
+            <ul className="dynamic-txts">
+              <li>
+                <span>Your mental health superhero, always by your side</span>
+              </li>
+              <li>
+                <span>Are You feeling too depressed and hopeless?</span>
+              </li>
+              <li>
+                <span>
+                  Our Therapist.Ai is always here to support and help You.
+                </span>
+              </li>
+            </ul>
+          </div>
+          <div className="prompt-container">
+            <textarea
+              placeholder="What's been troubling you? Let us help you feel better!"
+              className="prompt-box"
+              value={ask}
+              onChange={onUserChangeText}
+            />
+
+            <div className="prompt-buttons">
+              <a
+                className={
+                  isGenerating ? "generate-button loading" : "generate-button"
+                }
+                onClick={callGenerateEndpoint}
+              >
+                <div className="generate">
+                  {isGenerating ? <span className="loader"></span> : <p>Ask</p>}
+                </div>
+              </a>
             </div>
-            <div className="prompt-container">
-              <textarea
-                placeholder="What's been troubling you? Let us help you feel better!"
-                className="prompt-box"
-                value={age}
-                onChange={onUserChangeText}
-              />
-
-              <textarea
-                placeholder="Anything you want the song to be about..."
-                className="prompt-box"
-                value={ask}
-                onChange={onUserChangeText}
-              />
-
-              <div className="prompt-buttons">
-                <a
-                  className={
-                    isGenerating ? "generate-button loading" : "generate-button"
-                  }
-                  onClick={callGenerateEndpoint}
-                >
-                  <div className="generate">
-                    {isGenerating ? (
-                      <span className="loader"></span>
-                    ) : (
-                      <p>Ask</p>
-                    )}
-                  </div>
-                </a>
-              </div>
-              {/* Output */}
-              {apiOutput && (
-                <div className="output">
-                  <div className="output-header-container">
-                    <div className="output-header">
-                      <h3>Output</h3>
-                    </div>
-                  </div>
-                  <div className="output-content">
-                    <p>{apiOutput}</p>
+            {/* Output */}
+            {apiOutput && (
+              <div className="output">
+                <div className="output-header-container">
+                  <div className="output-header">
+                    <h3>Output</h3>
                   </div>
                 </div>
-              )}
-            </div>
+                <div className="output-content">
+                  <p>{apiOutput}</p>
+                </div>
+              </div>
+            )}
           </div>
+
           <div className="badge-container grow">
             <a
               href="https://twitter.com/Arghyad18"
