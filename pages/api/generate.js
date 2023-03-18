8556aa607287555d1d322d662e68376b82f5f63c
NemesisLW <86069181+NemesisLW@users.noreply.github.com>
2023-03-18 16:34:23 +0530

    Updated Generate.js - BasePrompt

---
+++ pages/api/generate.js
@@ -0,0 +1,28 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env["OPENAI_API_KEY"],
+});
+
+const openai = new OpenAIApi(configuration);
+
+const generateAction = async (req, res) => {
+  const basePromptPrefix = ` Write only json format as output with values "Solution" and "description" 
+  
+    Care: As an AI language model, I have a broad knowledge of human emotions, psychology, mental health conditions, and related topics. I am not a licensed mental health professional, and my advice should not replace the advice of a licensed professional. However, I can certainly offer guidance, tips, and resources that may be helpful to you. As a mental health advisor, I am here to help you navigate through your challenges and provide you with the best possible solutions. Please share your concerns and issues with me, and I will offer you practical advice, mental models, routines, habits, and even recommendations for medications if necessary. Remember, mental health is a crucial aspect of our well-being, and seeking help is a sign of strength, not weakness. So, feel free to open up and let's work together towards your healing and growth. 
+  
+    User: `;
+
+  const baseCompletion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.93,
+    max_tokens: 250,
+  });
+
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
