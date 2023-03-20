bf4b4292838a3c8271351a03e71dc0b868c1bb1e
NemesisLW <86069181+NemesisLW@users.noreply.github.com>
2023-03-20 09:15:47 +0530

    Formatted Code

--- pages/api/chatroomLink.js
+++ pages/api/chatroomLink.js
@@ -4,49 +4,49 @@ import { useRouter } from "next/router";
 // import "../styles.css";
 
 const Redir = ({ user }) => {
-	const router = useRouter();
-	// const [users, setUsers] = useState({});
-	// setUsers(user);
-	function createUserOnChatroom({ username, secret, email }) {
-		axios.put(
-			"https://api.chatengine.io/users/",
-			{ username, secret, email },
-			{
-				headers: {
-					"Private-key": `${process.env.NEXT_PUBLIC_PRIVATE_KEY}`,
-				},
-			}
-		);
-	}
-	function addUserToChatGroup({ username }) {
-		axios.post(
-			"https://api.chatengine.io/chats/{{chat_id}}/people/",
-			{ username },
-			{
-				headers: {
-					"Project-ID": `${process.env.NEXT_PUBLIC_PROJECT_ID}`,
-					"User-Name": `${process.env.NEXT_PUBLIC_USER_NAME}`,
-					"User-Secret": `${process.env.NEXT_PUBLIC_USER_SECRET}`,
-				},
-			}
-		);
-	}
+  const router = useRouter();
+  // const [users, setUsers] = useState({});
+  // setUsers(user);
+  function createUserOnChatroom({ username, secret, email }) {
+    axios.put(
+      "https://api.chatengine.io/users/",
+      { username, secret, email },
+      {
+        headers: {
+          "Private-key": `${process.env.NEXT_PUBLIC_PRIVATE_KEY}`,
+        },
+      }
+    );
+  }
+  function addUserToChatGroup({ username }) {
+    axios.post(
+      "https://api.chatengine.io/chats/{{chat_id}}/people/",
+      { username },
+      {
+        headers: {
+          "Project-ID": `${process.env.NEXT_PUBLIC_PROJECT_ID}`,
+          "User-Name": `${process.env.NEXT_PUBLIC_USER_NAME}`,
+          "User-Secret": `${process.env.NEXT_PUBLIC_USER_SECRET}`,
+        },
+      }
+    );
+  }
 
-	const handleSubmit = (user) => {
-		// createUserOnChatroom(user);
-		// addUserToChatGroup(user);
-		() => {
-			router.push("https://www.youtube.com/");
-		};
-	};
+  const handleSubmit = (user) => {
+    // createUserOnChatroom(user);
+    // addUserToChatGroup(user);
+    () => {
+      router.push("https://www.youtube.com/");
+    };
+  };
 
-	return (
-		<div className="prompt-buttons">
-			<div onClick={() => handleSubmit(user)} className="generate">
-				<h3>Join Groups</h3>
-			</div>
-		</div>
-	);
+  return (
+    <div className="prompt-buttons">
+      <div onClick={() => handleSubmit(user)} className="generate">
+        <h3>Join Groups</h3>
+      </div>
+    </div>
+  );
 };
 
 export default Redir;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,143 +4,142 @@ import Typewriter from "typewriter-effect";
 import Redir from "../pages/api/chatroomLink";
 
 const Home = () => {
-	// Perosonalized Data for better results
+  // Perosonalized Data for better results
 
-	const [age, setAge] = useState("");
-	const [gender, setGender] = useState("");
-	const [address, setAddress] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
+  const [address, setAddress] = useState("");
 
-	// Base user input for API call
-	const [ask, setAsk] = useState("");
+  // Base user input for API call
+  const [ask, setAsk] = useState("");
 
-	// API Call
+  // API Call
 
-	const [apiOutput, setApiOutput] = useState([]);
-	const [isGenerating, setIsGenerating] = useState(false);
-	const [render, setRender] = useState(false);
+  const [apiOutput, setApiOutput] = useState([]);
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [render, setRender] = useState(false);
 
-	// callGenerateEndpoint - provides the API with user input and generate output.
+  // callGenerateEndpoint - provides the API with user input and generate output.
 
-	const callGenerateEndpoint = async () => {
-		setIsGenerating(true);
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
 
-		console.log("Calling OpenAI...");
-		const response = await fetch("/api/generate", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ ask }),
-		});
+    console.log("Calling OpenAI...");
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ask }),
+    });
 
-		const data = await response.json();
-		const { output } = data;
-		console.log("OpenAI replied....");
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied....");
 
-		let solutions = JSON.parse(output);
+    let solutions = JSON.parse(output);
 
-		setApiOutput(solutions);
-		console.log(apiOutput);
-		setRender(true);
-		setIsGenerating(false);
-	};
+    setApiOutput(solutions);
+    console.log(apiOutput);
+    setRender(true);
+    setIsGenerating(false);
+  };
 
-	const onUserChangeText = (event) => {
-		setAsk(event.target.value);
-	};
+  const onUserChangeText = (event) => {
+    setAsk(event.target.value);
+  };
 
-	return (
-		<div className="root">
-			<Head>
-				<title>
-					DocCompanion - Your mental health superhero, always by your side
-				</title>
-			</Head>
-			<div className="bgr">
-				<div className="bgrin">
-					<div className="container">
-						<div className="animation">
-							<span className="first">Therapist</span>
-							<span className="slide">
-								<span className="second">AI</span>
-							</span>
-						</div>
-					</div>
-				</div>
-			</div>
+  return (
+    <div className="root">
+      <Head>
+        <title>
+          DocCompanion - Your mental health superhero, always by your side
+        </title>
+      </Head>
+      <div className="bgr">
+        <div className="bgrin">
+          <div className="container">
+            <div className="animation">
+              <span className="first">Therapist</span>
+              <span className="slide">
+                <span className="second">AI</span>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
 
-			<div className="containerc">
-				<div className="header-subtitle">
-					<Typewriter
-						options={{
-							strings: [
-								"Your mental health superhero, always by your side",
-								"Are you feeling the blues?",
-								"Therapist.ai got your back.",
-							],
-							autoStart: true,
-							loop: true,
-						}}
-					/>
+      <div className="containerc">
+        <div className="header-subtitle">
+          <Typewriter
+            options={{
+              strings: [
+                "Your mental health superhero, always by your side",
+                "Are you feeling the blues?",
+                "Therapist.ai got your back.",
+              ],
+              autoStart: true,
+              loop: true,
+            }}
+          />
 
-					<div className="prompt-container">
-						<textarea
-							placeholder="What's been troubling you? Let us help you feel better!"
-							className="prompt-box"
-							value={ask}
-							onChange={onUserChangeText}
-						/>
-						<div className="prompt-buttons">
-							<a
-								className={
-									isGenerating ? "generate-button loading" : "generate-button"
-								}
-								onClick={callGenerateEndpoint}
-							>
-								<div className="generate">
-									{isGenerating ? <span className="loader"></span> : <p>Ask</p>}
-								</div>
-							</a>
-						</div>
-						{/* Output */}
-						{render && (
-							<div className="output">
-								<div className="output-header-container">
-									<div className="output-header">
-										<h3>Probable Solutions</h3>
-									</div>
-								</div>
-								<div className="output-content">
-									{apiOutput.map((sol, index) => {
-										return (
-											<div key={index}>
-												<h2>{sol.solution}</h2>
-												<p>{sol.description}</p>
-											</div>
-										);
-									})}
-								</div>
-							</div>
-						)}
-						{/* <Redir user={user} /> */}
-						
-					</div>
+          <div className="prompt-container">
+            <textarea
+              placeholder="What's been troubling you? Let us help you feel better!"
+              className="prompt-box"
+              value={ask}
+              onChange={onUserChangeText}
+            />
+            <div className="prompt-buttons">
+              <a
+                className={
+                  isGenerating ? "generate-button loading" : "generate-button"
+                }
+                onClick={callGenerateEndpoint}
+              >
+                <div className="generate">
+                  {isGenerating ? <span className="loader"></span> : <p>Ask</p>}
+                </div>
+              </a>
+            </div>
+            {/* Output */}
+            {render && (
+              <div className="output">
+                <div className="output-header-container">
+                  <div className="output-header">
+                    <h3>Probable Solutions</h3>
+                  </div>
+                </div>
+                <div className="output-content">
+                  {apiOutput.map((sol, index) => {
+                    return (
+                      <div key={index}>
+                        <h2>{sol.solution}</h2>
+                        <p>{sol.description}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            )}
+            {/* <Redir user={user} /> */}
+          </div>
 
-					<div className="badge-container grow">
-						<a
-							href="https://github.com/NemesisLW/Therapist-AI"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<div className="badge">
-								<p>Identity Crisis</p>
-							</div>
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+          <div className="badge-container grow">
+            <a
+              href="https://github.com/NemesisLW/Therapist-AI"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="badge">
+                <p>Identity Crisis</p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
