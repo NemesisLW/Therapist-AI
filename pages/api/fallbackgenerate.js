8aa66eae0800bc4769d4a24306ddb3e6cd670d40
NemesisLW <86069181+NemesisLW@users.noreply.github.com>
2023-03-20 22:24:21 +0530

    Added JSON parsing error fallback

---
+++ pages/api/fallbackgenerate.js
@@ -0,0 +1,32 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+// generateAction - API call to OpenAI, Prompt designed to output in JSON format.(Though Output may be inconsistent, resulting into parsing error)
+const generatefallbackAction = async (req, res) => {
+  const basePromptPrefix = `Care: As an AI language model, I have a broad knowledge of human emotions, psychology, mental health conditions, and related topics. I am not a licensed mental health professional, and my advice should not replace the advice of a licensed professional. However, I can certainly offer guidance, tips, and resources that may be helpful to you. As a mental health advisor, I am here to help you navigate through your challenges and provide you with the best possible solutions. Please share your concerns and issues with me, and I will offer you practical advice, mental models, routines, habits, and even recommendations for medications if necessary.
+
+User:`;
+
+  console.log(`API: ${basePromptPrefix}${req.body.ask}`);
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      { role: "system", content: basePromptPrefix },
+      { role: "user", content: `${req.body.ask}${jsonOutput}` },
+    ],
+    max_tokens: 250,
+  });
+
+  console.log(completion.data.choices[0].message);
+  const basePromptOutput = completion.data.choices[0].message.content;
+  const textOutput = JSON.parse(JSON.stringify(basePromptOutput));
+  res.status(200).json({ output: textOutput });
+};
+
+export default generateAction;
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,168 +7,174 @@ import send from "../assets/sendmes.svg";
 import attach from "../assets/attach.svg";
 
 const Home = () => {
-	// Perosonalized Data for better results
+  // Perosonalized Data for better results
+  // const [age, setAge] = useState("");
+  // const [gender, setGender] = useState("");
+  // const [address, setAddress] = useState("");
 
-	const [age, setAge] = useState("");
-	const [gender, setGender] = useState("");
-	const [address, setAddress] = useState("");
+  // Base user input for API call
+  const [ask, setAsk] = useState("");
 
-	// Base user input for API call
-	const [ask, setAsk] = useState("");
+  // API Call
+  const [apiOutput, setApiOutput] = useState([]);
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [render, setRender] = useState(false);
 
-	// API Call
+  //userMessages gonna saved here
+  const [message, setMessage] = useState("");
 
-	const [apiOutput, setApiOutput] = useState([]);
-	const [isGenerating, setIsGenerating] = useState(false);
-	const [render, setRender] = useState(false);
+  // const [updated, setUpdated] = useState(message);
 
-	//userMessages gonna saved here
-	const [message, setMessage] = useState("");
+  const [msgListOfuser, setmsgListOfuser] = useState([]);
+  const [msgListOfbot, setmsgListOfbot] = useState([]);
+  const handleChange = (event) => {
+    setMessage(event.target.value);
+  };
 
-	// const [updated, setUpdated] = useState(message);
+  const handleClick = (e) => {
+    // 👇 "message" stores input field value
+    e.preventDefault();
+    // setUpdated(message);
+    // setmsgListOfuser(...msgListOfuser, updated);
+    setmsgListOfuser((msgListOfuser) => [...msgListOfuser, message]);
+    setMessage("");
+    //oldArray => [...oldArray, newElement]
+  };
 
-	const [msgListOfuser, setmsgListOfuser] = useState([]);
-	const handleChange = (event) => {
-		setMessage(event.target.value);
-	};
+  // callGenerateEndpoint - provides the API with user input and generate output.
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
 
-	const handleClick = (e) => {
-		// 👇 "message" stores input field value
-		e.preventDefault();
-		// setUpdated(message);
-		console.log(message);
-		// setmsgListOfuser(...msgListOfuser, updated);
-		setmsgListOfuser((msgListOfuser) => [...msgListOfuser, message]);
-		setMessage("");
-		//oldArray => [...oldArray, newElement]
-	};
+    console.log("Calling OpenAI...");
 
-	// callGenerateEndpoint - provides the API with user input and generate output.
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ask }),
+    });
 
-	const callGenerateEndpoint = async () => {
-		setIsGenerating(true);
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied....");
 
-		console.log("Calling OpenAI...");
-		const response = await fetch("/api/generate", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ ask }),
-		});
+    let solutions = JSON.parse(output);
 
-		const data = await response.json();
-		const { output } = data;
-		console.log("OpenAI replied....");
+    const fallbackResponse = await fetch("/api/fallbackgenerate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ask }),
+    });
 
-		let solutions = JSON.parse(output);
+    setApiOutput(solutions);
+    console.log(apiOutput);
+    setRender(true);
+    setIsGenerating(false);
+  };
 
-		setApiOutput(solutions);
-		console.log(apiOutput);
-		setRender(true);
-		setIsGenerating(false);
-	};
+  const onUserChangeText = (event) => {
+    setAsk(event.target.value);
+  };
 
-	const onUserChangeText = (event) => {
-		setAsk(event.target.value);
-	};
+  return (
+    <div className="root">
+      <Head>
+        <meta name="viewport" content="width=device-width initial-scale=1.0" />
+        <title>
+          Therapist.ai - Your mental health superhero, always by your side
+        </title>
+      </Head>
+      <div className="bgr">
+        <div className="bgrin">
+          <div className="container">
+            <div className="animation">
+              <span className="first">Therapist</span>
+              <span className="slide">
+                <span className="second">AI</span>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
 
-	return (
-		<div className="root">
-			<Head>
-				<meta name="viewport" content="width=device-width initial-scale=1.0" />
-				<title>
-					Therapist.ai - Your mental health superhero, always by your side
-				</title>
-			</Head>
-			<div className="bgr">
-				<div className="bgrin">
-					<div className="container">
-						<div className="animation">
-							<span className="first">Therapist</span>
-							<span className="slide">
-								<span className="second">AI</span>
-							</span>
-						</div>
-					</div>
-				</div>
-			</div>
+      <div className="containerc">
+        <div className="header-subtitle">
+          <Typewriter
+            options={{
+              strings: [
+                "Your mental health superhero, always by your side",
+                "Are you feeling the blues?",
+                "Therapist.ai got your back.",
+              ],
+              autoStart: true,
+              loop: true,
+            }}
+          />
 
-			<div className="containerc">
-				<div className="header-subtitle">
-					<Typewriter
-						options={{
-							strings: [
-								"Your mental health superhero, always by your side",
-								"Are you feeling the blues?",
-								"Therapist.ai got your back.",
-							],
-							autoStart: true,
-							loop: true,
-						}}
-					/>
+          <div className="prompt-container">
+            <textarea
+              placeholder="What's been troubling you? Let us help you feel better!"
+              className="prompt-box"
+              value={ask}
+              onChange={onUserChangeText}
+            />
+            <div className="prompt-buttons">
+              <a
+                className={
+                  isGenerating ? "generate-button loading" : "generate-button"
+                }
+                onClick={callGenerateEndpoint}
+              >
+                <div className="generate">
+                  {isGenerating ? <span className="loader"></span> : <p>Ask</p>}
+                </div>
+              </a>
+            </div>
+            {/* Output */}
+            {render && (
+              <div className="output">
+                <div className="output-header-container">
+                  <div className="output-header">
+                    <h3>Probable Solutions</h3>
+                  </div>
+                </div>
+                <div className="output-content">
+                  {apiOutput.map((sol, index) => {
+                    return (
+                      <div key={index}>
+                        <h2>{sol.solution}</h2>
+                        <p>{sol.description}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            )}
+            {/* <Redir user={user} /> */}
+          </div>
+          <div className="badge-container grow">
+            <a
+              href="https://github.com/NemesisLW/Therapist-AI"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="badge">
+                <p>Identity Crisis</p>
+              </div>
+            </a>
+          </div>
+        </div>
 
-					<div className="prompt-container">
-						<textarea
-							placeholder="What's been troubling you? Let us help you feel better!"
-							className="prompt-box"
-							value={ask}
-							onChange={onUserChangeText}
-						/>
-						<div className="prompt-buttons">
-							<a
-								className={
-									isGenerating ? "generate-button loading" : "generate-button"
-								}
-								onClick={callGenerateEndpoint}
-							>
-								<div className="generate">
-									{isGenerating ? <span className="loader"></span> : <p>Ask</p>}
-								</div>
-							</a>
-						</div>
-						{/* Output */}
-						{render && (
-							<div className="output">
-								<div className="output-header-container">
-									<div className="output-header">
-										<h3>Probable Solutions</h3>
-									</div>
-								</div>
-								<div className="output-content">
-									{apiOutput.map((sol, index) => {
-										return (
-											<div key={index}>
-												<h2>{sol.solution}</h2>
-												<p>{sol.description}</p>
-											</div>
-										);
-									})}
-								</div>
-							</div>
-						)}
-						{/* <Redir user={user} /> */}
-					</div>
-					<div className="badge-container grow">
-						<a
-							href="https://github.com/NemesisLW/Therapist-AI"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<div className="badge">
-								<p>Identity Crisis</p>
-							</div>
-						</a>
-					</div>
-				</div>
-
-				<div className="chatui">
-					<div className="chat">
-						<div className="newchat">
-							<p className="up">That friend</p>
-							<p className="down">Active 1.58 PM,Sat, Mar 18,2023</p>
-						</div>
-						{/* <div className="mainchat">
+        <div className="chatui">
+          <div className="chat">
+            <div className="newchat">
+              <p className="up">That friend</p>
+              <p className="down">Active 1.58 PM,Sat, Mar 18,2023</p>
+            </div>
+            {/* <div className="mainchat">
 							<div className="mainchatin">
 								<div className="chatbox">
 									<div className="message my_msg">
@@ -201,68 +207,74 @@ const Home = () => {
 							</div>
 						</div> */}
 
+            {/* user msgs goes here */}
 
-						{/* user msgs goes here */}
+            {msgListOfuser.map((eachMsg) => {
+              return (
+                <div className="message my_msg">
+                  <p>{eachMsg}</p>
+                </div>
+              );
+            })}
 
+            {/* Bot msgs goes here */}
 
-						{msgListOfuser.map((eachMsg) => {
-							return (
-								<div className="message my_msg">
-									<p>
-										{eachMsg}
-									</p>
-								</div>
-							);
-						})}
+            {msgListOfbot.map((eachMsg) => {
+              return (
+                <div className="message friend_msg">
+                  <p>{eachMsg}</p>
+                </div>
+              );
+            })}
 
-						{/* <form className="mainchatin"> */}
-						<div className="mainchat">
-							<div className="mainchatin">
-								<div className="chatbox">{}</div>
-							</div>
-						</div>
-						<div className="sendout">
-							<div className="sendin">
-								<div className="mes">
-									<input
-										className="textmsg"
-										type="text"
-										id="message"
-										name="message"
-										onChange={handleChange}
-										value={message}
-									/>
-								</div>
-								<div className="file">
-									<Image
-										className="attach"
-										src={attach}
-										alt="Picture of the author"
-										width="30px"
-										height="30px"
-									/>
-								</div>
-								<button
-									className="send"
-									type="submit"
-									onClick={(e) => handleClick(e)}
-								>
-									<Image
-										className="sendmsg"
-										src={send}
-										alt="Picture of the author"
-										width="30px"
-										height="30px"
-									/>
-								</button>
-							</div>
-						</div>
-						{/* </form> */}
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+            {/* <form className="mainchatin"> */}
+            <div className="mainchat">
+              <div className="mainchatin">
+                <div className="chatbox">{}</div>
+              </div>
+            </div>
+            <div className="sendout">
+              <div className="sendin">
+                <div className="mes">
+                  <input
+                    className="textmsg"
+                    type="text"
+                    id="message"
+                    name="message"
+                    onChange={handleChange}
+                    value={message}
+                  />
+                </div>
+                <div className="file">
+                  <Image
+                    className="attach"
+                    src={attach}
+                    alt="Picture of the author"
+                    width="30px"
+                    height="30px"
+                  />
+                </div>
+                <button
+                  className="send"
+                  type="submit"
+                  onClick={(e) => handleClick(e)}
+                >
+                  <Image
+                    className="sendmsg"
+                    src={send}
+                    alt="Picture of the author"
+                    width="30px"
+                    height="30px"
+                  />
+                </button>
+              </div>
+            </div>
+            {/* </form> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
