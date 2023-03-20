ceb8fdc56be2bf0fcd4e71bc3521dfb061b32e0c
NemesisLW <86069181+NemesisLW@users.noreply.github.com>
2023-03-21 04:23:21 +0530

    Fixed Custom Fonts Render

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/fonts/inter-var.woff2",
+        headers: [
+          {
+            key: "Cache-control",
+            value: "public, immutable, max-age=31536000",
+          },
+        ],
+      },
+    ];
+  },
+};
