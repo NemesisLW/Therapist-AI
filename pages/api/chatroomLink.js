22baa3e3f8a46e92b631bb75d01123b264b63467
Udit <uditde.com@gmail.com>
2023-03-25 00:22:51 +0530

    dynamic user input is ready

--- pages/api/chatroomLink.js
+++ pages/api/chatroomLink.js
@@ -4,13 +4,14 @@ import { useRouter } from "next/router";
 // import "../styles.css";
 
 const Redir = () => {
-	
-	const user = {
-		username: "udit",
-		secret: "pass123",
-		email: "uditde.com@gmail.com",
-	};
-	// const newMember = user.username;
+	const [user, setUser] = useState({
+		username: "",
+		email: "",
+		secret: "",
+	});
+	// const [userName, setUserName] = useState("");
+	// const [email, setEmail] = useState("");
+	// const [secret, setSecret] = useState("");
 	const router = useRouter();
 	// const [users, setUsers] = useState({});
 	// setUsers(user);
@@ -41,28 +42,88 @@ const Redir = () => {
 				}
 			)
 			.then((r) => callback(r.data))
-			.then(() => router.push("https://therapist-ai-chat-room.netlify.app/chats"));
+			.then(() => router.push("https://chat-room-uditde.vercel.app/"));
 	}
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		// createUserOnChatroom(user);
-		// addUserToChatGroup(user);
-		// () => {
-		// 	router.push("https://www.youtube.com/");
-		// };
+	const handleChange = (e) => {
+		//handles the changes of each input
+		const name = e.target.name;
+		const value = e.target.value;
+		setUser((values) => ({ ...values, [name]: value }));
+	};
 
+	const handleSubmit = (e) => {
+		//handles the submission of user details
+		e.preventDefault();
+		console.log(user);
 		createUserOnChatroom((user) => {
 			addUserToChatGroup((chat) => console.log("success!", chat));
 		});
-		// router.push("https://www.youtube.com/");
 	};
 
+	const [joiningForm, setJoining] = useState(false);
+
 	return (
-		<div className="prompt-buttons">
-			<div onClick={(e) => handleSubmit(e)} className="generate">
+		<div className="join">
+			{/* <div onClick={(e) => handleSubmit(e)} className="generate">
 				<h3>Join Groups</h3>
-			</div>
+			</div> */}
+
+			{!joiningForm ? (
+				<div className="prompt-container" id="prompt-container-id">
+					<div
+						onClick={(e) => setJoining(true)}
+						className="prompt-buttons"
+						id="prompt-buttons-id"
+					>
+						<div className="generate" id="generateid">
+							<div className="generate-button" id="join-to-our-community">
+								Join to our community Groups
+							</div>
+						</div>
+					</div>
+				</div>
+			) : (
+				<div className="prompt-container">
+					<form className="prompt-container">
+						<input
+							value={user.username || ""}
+							onChange={handleChange}
+							placeholder="username"
+							type="text"
+							name="username"
+							className="prompt-box"
+							required
+						/>
+						<input
+							value={user.email || ""}
+							onChange={handleChange}
+							placeholder="Email address"
+							type="email"
+							name="email"
+							className="prompt-box"
+							required
+						/>
+						<input
+							value={user.secret || ""}
+							onChange={handleChange}
+							placeholder="Password"
+							type="password"
+							name="secret"
+							className="prompt-box"
+							required
+						/>
+						<div
+							type="submit"
+							className="prompt-buttons"
+							id="submit"
+							onClick={(e) => handleSubmit(e)}
+						>
+							<div className="generate">Submit</div>
+						</div>
+					</form>
+				</div>
+			)}
 		</div>
 	);
 };
