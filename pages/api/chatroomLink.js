b01cba488756df3a8989c8c19db77b72c1aa7ec8
Arghya Dutta <86069181+NemesisLW@users.noreply.github.com>
2023-03-24 14:49:14 +0530

    Update chatroomLink.js

--- pages/api/chatroomLink.js
+++ pages/api/chatroomLink.js
@@ -5,43 +5,44 @@ import { useRouter } from "next/router";
 
 const Redir = () => {
 	
-	const user = {
-		username: "udit",
-		secret: "pass123",
-		email: "uditde.com@gmail.com",
-	};
-	// const newMember = user.username;
-	const router = useRouter();
-	// const [users, setUsers] = useState({});
-	// setUsers(user);
-	function createUserOnChatroom(callback) {
-		axios
-			.put(
-				"https://api.chatengine.io/users/",
-				{ ...user },
-				{
-					headers: {
-						"Private-key": `${process.env.NEXT_PUBLIC_PRIVATE_KEY}`,
-					},
-				}
-			)
-			.then((r) => callback(r.data));
-	}
-	function addUserToChatGroup(callback) {
-		axios
-			.post(
-				"https://api.chatengine.io/chats/151900/people/",
-				{ username: `${user.username}` },
-				{
-					headers: {
-						"Project-ID": `${process.env.NEXT_PUBLIC_PROJECT_ID}`,
-						"User-Name": `${process.env.NEXT_PUBLIC_USER_NAME}`,
-						"User-Secret": `${process.env.NEXT_PUBLIC_USER_SECRET}`,
-					},
-				}
-			)
-			.then((r) => callback(r.data))
-			.then(() => router.push("https://chat-room-j.vercel.app/"));
+// 	const user = {
+// 		username: "udit",
+// 		secret: "pass123",
+// 		email: "uditde.com@gmail.com",
+// 	};
+// 	// const newMember = user.username;
+// 	const router = useRouter();
+// 	// const [users, setUsers] = useState({});
+// 	// setUsers(user);
+// 	function createUserOnChatroom(callback) {
+// 		axios
+// 			.put(
+// 				"https://api.chatengine.io/users/",
+// 				{ ...user },
+// 				{
+// 					headers: {
+// 						"Private-key": `${process.env.NEXT_PUBLIC_PRIVATE_KEY}`,
+// 					},
+// 				}
+// 			)
+// 			.then((r) => callback(r.data));
+// 	}
+// 	function addUserToChatGroup(callback) {
+// 		axios
+// 			.post(
+// 				"https://api.chatengine.io/chats/151900/people/",
+// 				{ username: `${user.username}` },
+// 				{
+// 					headers: {
+// 						"Project-ID": `${process.env.NEXT_PUBLIC_PROJECT_ID}`,
+// 						"User-Name": `${process.env.NEXT_PUBLIC_USER_NAME}`,
+// 						"User-Secret": `${process.env.NEXT_PUBLIC_USER_SECRET}`,
+// 					},
+// 				}
+// 			)
+// 			.then((r) => callback(r.data))
+// 			.then(() => 
+	router.push("https://chat-room-j.vercel.app/"));
 	}
 
 	const handleSubmit = (e) => {
