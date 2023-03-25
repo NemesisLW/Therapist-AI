3317dd0b81465d069467aebb847eb9966e7bf87f
Arghya Dutta <86069181+NemesisLW@users.noreply.github.com>
2023-03-25 18:18:32 +0530

    Update chatbotgenerate.js

--- pages/api/chatbotgenerate.js
+++ pages/api/chatbotgenerate.js
@@ -19,7 +19,7 @@ const generatebotAction = async (req, res) => {
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0.6,
-    stop: ["\n", "->"],
+    stop: ["\n", "->", "User"],
   });
 
   const basePromptOutput = baseCompletion.data.choices.pop();
